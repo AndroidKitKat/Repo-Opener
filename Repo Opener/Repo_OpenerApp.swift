93eeac29cf0fdbe9bbaf50f5bc9e2881ee56ca93
Mike E <me@michaeleisemann.com>
2021-03-25 21:36:36 -0400

    Initial Commit

---
+++ Repo Opener/Repo_OpenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Repo_OpenerApp.swift
+//  Repo Opener
+//
+//  Created by skg on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Repo_OpenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
